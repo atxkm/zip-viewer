07092636437df077d842d8f78bf935dbb1a69eef
汤晓奎 <tangxiaokui@tedu.cn>
2019-11-05 08:52:46 +0800

    文件样式

--- src/app/pages/file/file.component.ts
+++ src/app/pages/file/file.component.ts
@@ -1,27 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-file-info',
   templateUrl: './file.component.html',
   styleUrls: ['./file.component.scss'],
 })
-export class FileComponent implements OnInit {
+export class FileComponent implements OnInit, AfterViewInit {
 
-  view = 'list';
+  @ViewChild('tree', { static: false }) tree;
+
+  view = 'block';
   searchValue;
   nodes = [
     {
-      title: 'parent 1',
-      key: '100',
-      expanded: true,
-      selectable: false,
+      title: 'storage', key: '100', expanded: true,
       children: [
-        { title: 'leaf', key: '1001', icon: 'anticon anticon-meh-o', isLeaf: true },
-        { title: 'leaf', key: '1002', icon: 'anticon anticon-frown-o', isLeaf: true },
-        { title: 'leaf', key: '1003', icon: 'anticon anticon-frown-o', isLeaf: true },
+        {
+          title: 'emulated', key: '1000', expanded: true,
+          children: [
+            {
+              title: '0', key: '10000', expanded: true,
+              children: [
+                {
+                  title: 'DCIM', key: '100000', expanded: true,
+                  children: [
+                    {
+                      title: 'Camera', key: '1000000', expanded: true,
+                      children: [
+                        { title: '20191029_141408.mp4', key: '10000000', icon: 'video-camera', isLeaf: true, size: '20M', date: '2019-11-01', type: '视频' },
+                        { title: '20191029_141412.mp4', key: '10000001', icon: 'video-camera', isLeaf: true, size: '30M', date: '2019-11-01', type: '视频' },
+                        { title: '20191029_141123.mp4', key: '10000002', icon: 'video-camera', isLeaf: true, size: '40M', date: '2019-11-01', type: '视频' },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
       ],
     },
   ];
+  files;
+  file;
 
   constructor() {
   }
@@ -29,11 +50,30 @@ export class FileComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => this.files = this.tree.getTreeNodes());
+  }
+
   onClickNode(e) {
     const node = e.node;
-    console.log(e);
-    if (!node.isLeaf) {
-      node.isExpanded = !node.isExpanded;
+    if (node.isLeaf) {
+      this.files = [node];
+      this.file = node;
+    } else {
+      this.files = node.children;
+    }
+  }
+
+  onSelect(item) {
+    if (item.isLeaf) {
+      this.file = item;
+    } else {
+      this.files = item.children;
+    }
+    item.isSelected = true;
+    while (item.parentNode) {
+      item.parentNode.isExpanded = true;
+      item = item.parentNode;
     }
   }
 
